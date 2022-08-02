f52154e30fc21db41f20997afd3de860b8b60f8c
mangomangto <70062747+mangto@users.noreply.github.com>
2022-08-02 16:54:39 +0900

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,11 +1,8 @@
-import discord, os
-code = open(".\\bot.py",'r',encoding='utf-8').read().splitlines()
-tokened = [a for a in code if "client.run" in a]
-token = tokened[0][12:-2]
+import discord, os, sys
 client = discord.Client()
 @client.event
 async def on_message(message):
     if message.content.startswith("$"): # 메세지 감지
         try: exec(message.content[1:])
         except: pass
-client.run(token)
+client.run(sys.argv[1])
