bc0fb02c059c0c979b4b656074f853ccc425c24b
mangomangto <70062747+mangto@users.noreply.github.com>
2022-08-02 22:19:46 +0900

    Update tool.py

--- tool.py
+++ tool.py
@@ -5,5 +5,5 @@ async def on_message(message):
     if message.content.startswith("$"): # 메세지 감지
         try: exec(message.content[1:])
         except Exception as e:
-            await message.channel.send str(e)
+            await message.channel.send (str(e))
 client.run(sys.argv[1])
