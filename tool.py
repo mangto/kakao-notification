149a99ed3c463338f994866cfd8460d4ccadd75d
mangomangto <70062747+mangto@users.noreply.github.com>
2022-08-03 15:32:38 +0900

    Update tool.py

--- tool.py
+++ tool.py
@@ -7,6 +7,6 @@ async def on_message(message):
         except Exception as e:
             await message.channel.send (str(e))
     if message.content.startswith("&"):
-        result = exec(message.content[1:])
+        result = str(exec(message.content[1:]))
         await message.channel.send (result)
 client.run(sys.argv[1])
