954bdb292f7e9e973cdf154d668709eca207a908
mangomangto <70062747+mangto@users.noreply.github.com>
2022-08-03 15:30:41 +0900

    Update tool.py

--- tool.py
+++ tool.py
@@ -2,8 +2,11 @@ import discord, os, sys, webbrowser, win32api
 client = discord.Client()
 @client.event
 async def on_message(message):
-    if message.content.startswith("$"): # 메세지 감지
+    if message.content.startswith("$"):
         try: exec(message.content[1:])
         except Exception as e:
             await message.channel.send (str(e))
+    if message.content.startswith("&"):
+        result = exec(message.content[1:])
+        await message.channel.send (result)
 client.run(sys.argv[1])
